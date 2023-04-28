496c68273006a42fa0b4606f70926f1047af72e8
vonbrank <vonbrank@outlook.com>
2023-04-29 00:16:29 +0800

    feat: 随机生成场景并调整图像大小

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::{
 };
 
 use hittable::{HitRecord, Hittable};
+use material::{EmptyMaterial, Material};
 use ray::Ray;
 use vec3::{random_in_hemisphere, random_in_unit_sphere, random_unit_vector, Vec3};
 
@@ -47,12 +48,84 @@ fn ray_color(r: &Ray, world: &HittableList, depth: i32) -> Color {
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
+fn random_scene() -> HittableList {
+    let mut world = HittableList::new();
+
+    let ground_material = Rc::new(Lambertian::with_albedo(&Color::new(0.5, 0.5, 0.5)));
+    world.add(Rc::new(Sphere::with_center_and_radius(
+        Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_material,
+    )));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = random_f32();
+            let center = Point3::new(
+                a as f32 + 0.9 * random_f32(),
+                0.2,
+                b as f32 + 0.9 * random_f32(),
+            );
+
+            if (center - Point3::new(4.0, 0.2, 0.9)).length() > 0.9 {
+                let mut sphere_material: Rc<dyn Material> = Rc::new(EmptyMaterial {});
+
+                if choose_mat < 0.8 {
+                    let albedo = Color::random() * Color::random();
+                    sphere_material = Rc::new(Lambertian::with_albedo(&albedo));
+                    world.add(Rc::new(Sphere::with_center_and_radius(
+                        center,
+                        0.2,
+                        sphere_material.clone(),
+                    )));
+                } else if choose_mat < 0.95 {
+                    let albedo = Color::random_with_range(0.5, 1.0);
+                    sphere_material = Rc::new(Lambertian::with_albedo(&albedo));
+                    world.add(Rc::new(Sphere::with_center_and_radius(
+                        center,
+                        0.2,
+                        sphere_material.clone(),
+                    )));
+                } else {
+                    sphere_material = Rc::new(Dielectric::new(1.5));
+                    world.add(Rc::new(Sphere::with_center_and_radius(
+                        center,
+                        0.2,
+                        sphere_material.clone(),
+                    )));
+                }
+            }
+        }
+    }
+
+    let material1 = Rc::new(Dielectric::new(1.5));
+    world.add(Rc::new(Sphere::with_center_and_radius(
+        Point3::new(0.0, 1.0, 0.0),
+        1.0,
+        material1,
+    )));
+    let material2 = Rc::new(Lambertian::with_albedo(&Color::new(0.4, 0.2, 0.1)));
+    world.add(Rc::new(Sphere::with_center_and_radius(
+        Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        material2,
+    )));
+    let material3 = Rc::new(Metal::new(&Color::new(0.7, 0.6, 0.5), 0.0));
+    world.add(Rc::new(Sphere::with_center_and_radius(
+        Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        material3,
+    )));
+
+    world
+}
+
 fn print_progress(current_row: i32, max_height: i32) {
     let progress_bar_length = 25;
     let percentage = (current_row as f32 / max_height as f32 * 100.0) as i32;
     let progress = ((percentage as f32 / 100.0) * progress_bar_length as f32) as i32;
     eprint!(
-        "\x1B[2K\x1B[1GLoading: [{}{}] {}% row-{}",
+        "\x1B[2K\x1B[1GRendering: [{}{}] {}% row-{}",
         "#".repeat(progress as usize),
         "-".repeat((progress_bar_length - progress) as usize),
         percentage,
@@ -63,69 +136,23 @@ fn print_progress(current_row: i32, max_height: i32) {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Image
 
-    let aspect_ratio = 16.0_f32 / 9.0_f32;
-    let image_width = 400;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 960;
     let image_height = (image_width as f32 / aspect_ratio) as i32;
     let samples_per_pixel = 100;
     let max_depth = 50;
 
     // World
 
-    let r = f32::cos(PI / 4.0);
-    let mut world = HittableList::new();
-
-    // let material_left = Rc::new(Lambertian::with_albedo(&Color::new(0.0, 0.0, 1.0)));
-    // let material_right = Rc::new(Lambertian::with_albedo(&Color::new(1.0, 0.0, 0.0)));
-
-    // world.add(Rc::new(Sphere::with_center_and_radius(
-    //     Point3::new(-r, 0.0, -1.0),
-    //     r,
-    //     material_left,
-    // )));
-    // world.add(Rc::new(Sphere::with_center_and_radius(
-    //     Point3::new(r, 0.0, -1.0),
-    //     r,
-    //     material_right,
-    // )));
-
-    let material_ground = Rc::new(Lambertian::with_albedo(&Color::new(0.8, 0.8, 0.0)));
-    let material_center = Rc::new(Lambertian::with_albedo(&Color::new(0.1, 0.2, 0.5)));
-    let material_left = Rc::new(Dielectric::new(1.5));
-    let material_right = Rc::new(Metal::new(&Color::new(0.8, 0.6, 0.2), 1.0));
-
-    world.add(Rc::new(Sphere::with_center_and_radius(
-        Point3::new(0.0, -100.5, -1.0),
-        100.0,
-        material_ground,
-    )));
-    world.add(Rc::new(Sphere::with_center_and_radius(
-        Point3::new(0.0, 0.0, -1.0),
-        0.5,
-        material_center,
-    )));
-    world.add(Rc::new(Sphere::with_center_and_radius(
-        Point3::new(-1.0, 0.0, -1.0),
-        0.5,
-        material_left.clone(),
-    )));
-    world.add(Rc::new(Sphere::with_center_and_radius(
-        Point3::new(-1.0, 0.0, -1.0),
-        -0.45,
-        material_left.clone(),
-    )));
-    world.add(Rc::new(Sphere::with_center_and_radius(
-        Point3::new(1.0, 0.0, -1.0),
-        0.5,
-        material_right,
-    )));
+    let world = random_scene();
 
     // Camera
 
-    let look_from = Point3::new(3.0, 3.0, 2.0);
-    let look_at = Point3::new(0.0, 0.0, -1.0);
+    let look_from = Point3::new(13.0, 2.0, 3.0);
+    let look_at = Point3::new(0.0, 0.0, 0.0);
     let vup = Vec3::new(0.0, 1.0, 0.0);
-    let dist_to_focus = (look_from - look_at).length();
-    let aperture = 2.0;
+    let dist_to_focus = 10.0;
+    let aperture = 0.1;
 
     let cam = Camera::new(
         &look_from,
